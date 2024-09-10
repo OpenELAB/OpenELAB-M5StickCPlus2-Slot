938dac44248e731b73c06b275ff302df8ca3c583
OpenELAB <info@openelab.io>
2024-09-10 15:07:01 +0800

    Update M5StickCPlus2_slot.ino

--- M5StickCPlus2_slot.ino
+++ M5StickCPlus2_slot.ino
@@ -10,91 +10,95 @@
 #define FLUSH_COUNT 3
 
 Slot slots[SLOT_COUNT];
-const int symbolIndices[] = { 2, 4, 5, 0, 3, 4, 2, 5, 3, 1, 0};//列图标显示顺序  每一列显示的图标相同
+const int symbolIndices[] = { 2, 4, 5, 0, 3, 4, 2, 5, 3, 1, 0};//Column icon display order Each column displays the same icons
 
 enum SlotsState { SLOTS_INIT, SLOTS_START, SLOTS_STOP = SLOT_COUNT + 1, SLOTS_FLUSH };
-int state = SLOTS_INIT;//老虎机状态值     设置为初始状态
+int state = SLOTS_INIT;//Slot machine state value Set to initial state
 
 //
-// setup()//初始化函数
+// setup()//Initialization Functions
 //
+
 void setup() {
- 	M5.begin();//初始化M5
-	M5.Lcd.setRotation(3);
-	M5.Lcd.fillScreen(TFT_BLACK);//设置屏幕背景色为黑色
-	M5.Lcd.setSwapBytes(true);
+   M5.begin();
+  M5.Lcd.setRotation(3);
+  M5.Lcd.fillScreen(TFT_BLACK);//Set the screen background color to black
+  M5.Lcd.setSwapBytes(true);
 
-
-	Slot::initShadow();//初始化阴影
-	Slot::setReel(symbolIndices, _countof(symbolIndices));//设置初始转盘
-	for (int i = 0; i < SLOT_COUNT; i++) {
-		slots[i].init(i, i * SLOT_COUNT);
-		slots[i].draw();
-	}
+  Slot::initShadow();//Initialize shadows
+  Slot::setReel(symbolIndices, _countof(symbolIndices));//Setting the initial dial
+  for (int i = 0; i < SLOT_COUNT; i++) {
+    slots[i].init(i, i * SLOT_COUNT);
+    slots[i].draw();
+  }
 }
 
 //
-// loop()
-//
+// loop()//main function, this function because you need to refresh the slot machine LCD 
+// at all times so there can not be a delay similar to delay_ms
+
 void loop() {
-	unsigned long tick = millis();//获得当前心跳值
-	static unsigned long flushTick;
-	static int flushCount;
+  unsigned long tick = millis();//Get the current heartbeat value
+  static unsigned long flushTick;
+  static int flushCount;
 
-	M5.update();
+  M5.update();
+  if (M5.BtnA.wasPressed()) {//Key Trigger
+    if (state == SLOTS_INIT) {//If it's in the initial state, start spinning the slot machine.
+      for (int i = 0; i < SLOT_COUNT; i++) {
+        slots[i].start();
+      }
+      state++;
+    } else if (state < SLOTS_STOP) {// is on and not in pause, stops a column
+      slots[state - 1].stop();
+      state++;//Increment to the second column
+    }
+  }
+  if (state == SLOTS_STOP) {	// The slot machine is in a stopped state, initializing the relevant 
+	  			//data to bring the slot machine back to its initial state.
+    int symbol = -1;
+    bool stopAll = true;
+    for (int i = 0; i < SLOT_COUNT; i++) {
+      int n = slots[i].getSymbol();
+      if (n == -1) {
+        stopAll = false;
+      } else {
+        symbol = (n == symbol || symbol == -1) ? n : -2;
+      }
+    }
+    if (stopAll) {
+      if (symbol >= 0) {
+        flushTick = tick;
+        flushCount = 0;
+        state = SLOTS_FLUSH;
+      } else {
+        state = SLOTS_INIT;
+      }
+    }
+  }
+ 
+  if (state == SLOTS_FLUSH) {//When all columns are stopped
+    if (tick >= flushTick + FLUSH_DELAY) {
+      flushTick = tick;
+      for (int i = 0; i < SLOT_COUNT; i++) {
+        slots[i].flush((flushCount & 1) ? TFT_WHITE : TFT_BLUE);//If all columns have the same icon, the background 
+	      							//will be blue otherwise unchanged.
+      }
+      if (++flushCount >= FLUSH_COUNT * 2) {
+        state = SLOTS_INIT;
+      }
+    }
+  }
 
-	if (M5.BtnA.wasPressed()) {
-		if (state == SLOTS_INIT) {
-			for (int i = 0; i < SLOT_COUNT; i++) {
-				slots[i].start();
-			}
-			state++;
-		} else if (state < SLOTS_STOP) {
-			slots[state - 1].stop();
-			state++;
-		}
-	}
-	if (state == SLOTS_STOP) {
-		int symbol = -1;
-		bool stopAll = true;
-		for (int i = 0; i < SLOT_COUNT; i++) {
-			int n = slots[i].getSymbol();
-			if (n == -1) {
-				stopAll = false;
-			} else {
-				symbol = (n == symbol || symbol == -1) ? n : -2;
-			}
-		}
-		if (stopAll) {
-			if (symbol >= 0) {
-				flushTick = tick;
-				flushCount = 0;
-				state = SLOTS_FLUSH;
-			} else {
-				state = SLOTS_INIT;
-			}
-		}
-	}
-	if (state == SLOTS_FLUSH) {
-		if (tick >= flushTick + FLUSH_DELAY) {
-			flushTick = tick;
-			for (int i = 0; i < SLOT_COUNT; i++) {
-				slots[i].flush((flushCount & 1) ? TFT_WHITE : TFT_BLUE);
-			}
-			if (++flushCount >= FLUSH_COUNT * 2) {
-				state = SLOTS_INIT;
-			}
-		}
-	}
+  for (int i = 0; i < SLOT_COUNT; i++) {//If all columns have the same icon, 
+	  				//the background will be blue otherwise unchanged.
+    if (slots[i].update()) {
+      slots[i].draw();
+    }
+  }
 
-	for (int i = 0; i < SLOT_COUNT; i++) {
-		if (slots[i].update()) {
-			slots[i].draw();
-		}
-	}
-
-	int ms = millis() - tick;
-	if (ms < LOOP_WAIT) {
-		delay(LOOP_WAIT - ms);
-	}
+  int ms = millis() - tick;
+  if (ms < LOOP_WAIT) {
+    delay(LOOP_WAIT - ms);
+  }
 }
